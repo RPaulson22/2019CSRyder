4f24832926891da09ade7d73b95ad9988a85fc63
RPaulson22 <rpaulson22@nscds.org>
2020-01-21 21:12:06 +0000

    Day 2

---
+++ challenge_1.js
@@ -0,0 +1,24 @@
+//challenge #1
+
+console.log('the first challenge :0');
+function centuryFromYear(year) {
+    return Math.ceil(year/100)
+}
+
+console.log(centuryFromYear(1905));
+console.log(centuryFromYear(1700));
+
+//challenge #2
+
+console.log('the second challenge :0');
+function reverse(message) {
+    var reverseString = "";
+    for(var i=message.length-1; i>=0; i--) {
+        reverseString += message.charAt(i);
+    }
+    return reverseString;
+}
+
+console.log(reverse("hello"));
+console.log(reverse("good"));
+console.log(reverse("tacocat"))
