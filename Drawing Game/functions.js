8a100f3079a2fd1095c08dad1b39ce782b91bc24
RPaulson22 <rpaulson22@nscds.org>
2020-02-06 17:33:57 +0000

    adaskdls

--- Drawing Game/functions.js
+++ Drawing Game/functions.js
@@ -1,2 +1,10 @@
 var canvas = document.getElementById('mainCanvas');
 var ctx = canvas.getContext('2d');
+ctx.fillStyle = "0000000";
+ctx.beginPath();
+ctx.arc( 300, 300, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+function drawStroke(canvas, ctx) {
+
+}
